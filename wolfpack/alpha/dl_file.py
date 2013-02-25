1ecd909540f0c3dd9733b96049fcb469652c341f
Sina Samavati <contact@s1n4.com>
2013-02-25 19:57:21 +0330

    Fix a lot of indentation issues

--- wolfpack/alpha/dl_file.py
+++ wolfpack/alpha/dl_file.py
@@ -14,33 +14,34 @@ class DLFile(object):
         self.downloading_chunks = list()
 
         r = requests.head(url)
-‌        self.size = int(r.headers['content-length'])
+        self.size = int(r.headers['content-length'])
         self.headers = r.headers    # TODO: Is it needed?
 
         self.calculate_ranges()
-		self.num_part=0 #count chunks
+        self.num_part=0 #count chunks
 
     def calculate_ranges(self):
         if self.size < settings.CHUNK_SIZE:
             self.available_chunks.append("%d-%d" %(0, self.size - 1))  #just one part to download
 
         self.num_part = self.size / settings.CHUNK_SIZE
-		
+
         for i in range(self.num_part):
-            self.available_chunks.append((i * settings.CHUNK_SIZE, 
+            self.available_chunks.append((i * settings.CHUNK_SIZE,
                                           ((i + 1) * settings.CHUNK_SIZE) - 1))
         if not self.size % settings.CHUNK_SIZE:
             self.available_chunks.append((self.num_part * settings.CHUNK_SIZE,
                                           self.size - 1))
-			self.num_part+=1
+            self.num_part+=1
+
     def request_chunk(self):
  #       for item in self.available_chunks:
   #          self.downloading_chunks.append(
-   #             self.available_chunks.pop(self.available_chunks.index(item)) 
-    #        ) 
-		for i in range(1,self.num_part):
-			self.downloading_chunks.append(
-				self.available_chunks.pop(i)
+   #             self.available_chunks.pop(self.available_chunks.index(item))
+    #        )
+        for i in range(1,self.num_part):
+            self.downloading_chunks.append(self.available_chunks.pop(i))
+
             return [self.url, self.downloading_chunks[-1][0], self.downloading_chunks[-1][1]]
 
         if not len(self.available_chunks):
@@ -56,4 +57,3 @@ class DLFile(object):
         for i in range(self.num_part):
             shutil.copyfileobj(open("%s.%d" % (self.name, i), 'rb'), final_file)
         final_file.close()
-
