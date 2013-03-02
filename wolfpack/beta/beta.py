2ecd98624755bc63262c240cb0369081a49d8deb
Afshin rodgar <afshin.chichi@gmail.com>
2013-03-02 16:32:21 +0330

    solve some bugs in beta

--- wolfpack/beta/beta.py
+++ wolfpack/beta/beta.py
@@ -4,7 +4,7 @@ import socket
 import time
 import requests
 import threading
-from wolfpack.lib.alpha_addr import get_alpha_addr
+#from wolfpack.lib.alpha_addr import get_alpha_addr
 import wolfpack.beta.settings as settings
 
 class Beta(object):
@@ -18,7 +18,7 @@ class Beta(object):
         self.socket_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket_.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         #self.socket_.connect(get_alpha_addr()) #TODO: I couldn't get nmap to work.
-	self.socket_.connect(('192.168.42.168', 54322))
+	self.socket_.connect((settings.HOST, settings.PORT))
         print 'Connected to the Alpha successfully.'
         print 'Waiting for further instructions.'
         #self.socket_.sendall("%s|%s"%(settings.USER, settings.PASS)) # verify username and password
@@ -26,27 +26,25 @@ class Beta(object):
         self.send_request()
 
     def receive(self):
-        r = requests.get(self.curr_url, stream=True, headers={'range': "bytes=%s" % self.range_}) #to change for next version
+        req = requests.get(self.curr_url, stream=True, headers={'range': "bytes=%s" % self.range_}) #to change for next version
 
         time.sleep(1)   # Wait for it to get a few bytes.
 
-        ok = '1' if r.ok else '0'
+        ok = '1' if req.ok else '0'
         self.socket_.sendall(ok)
 
         #for repairing the reset by peer Error
         #I Broke package in 10 small package
         #and send It to Beta_agent
         pre_time = time.time();
+        pack_size = settings.PACKAGE_SIZE / settings.PACKAGE_PEIS
         for packet in req.iter_content(chunk_size = settings.PACKAGE_SIZE):
-            pre_time = new_time
-            for i in range(setting.PACKAGE_PIECES):
-                bytes_ = packet.read(setting.PACKAGE_SIZE / setting.PACKAGE_PIECES))
+            new_time = time.time()
+            for i in range(settings.PACKAGE_PEIS):
+                bytes_ = packet[i * pack_size: (i+1) * pack_size]
                 self.socket_.sendall(bytes_)
             self.current_file_lenght += len(packet)
-            self.current_speed = (settings.PACKAGE_SIZE / 1024.0 / (new_time - pre_time))  #KBps
-
-#        data_file = r.raw
-#        total_bytes = self.end - self.start
+            self.current_speed = (settings.PACKAGE_SIZE / 1024.0 / (new_time - pre_time))  #KBps #        data_file = r.raw #        total_bytes = self.end - self.start
 #
 #        current = 0
 #        while current < total_bytes:
